4d4da8d3d0cb00a75038669dbff01327232b9b49
Mauro Gomez <mauro.gomez@oracle.com>
2021-10-29 11:28:11 -0300

    20211029

--- application/render/svg_builder.py
+++ application/render/svg_builder.py
@@ -36,7 +36,7 @@ PROPORTION_BOX_XY_OFFSETS = {
     }
 }
 
-def render_box(box_dict, box_x_offset, box_y_offset, box_height, box_width, write_original_text):
+def render_box(box_dict, box_x_offset, box_y_offset, box_height, box_width, text_height_proportion, write_original_text):
 
     box_string = box_dict['box']
     box_desc = box_dict['desc']
@@ -57,11 +57,13 @@ def render_box(box_dict, box_x_offset, box_y_offset, box_height, box_width, writ
 
     if write_original_text:
         decoded_box_desc = unidecode.unidecode(box_desc)
-        output += '<text x="' + str(box_x_offset + round(box_width / 4) ) + '" y="' + str(box_y_offset + round(box_height * 0.8)) + '" font-size="' + str(round(box_height / 6)) + '">' + decoded_box_desc + '</text>'
+        output += '<text x="' + str(box_x_offset + round(box_width / 4) ) + '" y="' + str(box_y_offset + round(box_height * 0.9)) + '" font-size="' + str(round(box_height * text_height_proportion )) + '">' + decoded_box_desc + '</text>'
 
     return output
 
-def render_pages(box_list, box_height, boxes_per_row, rows_per_page, left_margin, top_margin, write_original_text=True):
+def render_pages(box_list, box_height, boxes_per_row, rows_per_page, left_margin, top_margin, text_height_percentage, write_original_text=True):
+
+    text_height_proportion = text_height_percentage / 100
 
     book = []
 
@@ -81,7 +83,7 @@ def render_pages(box_list, box_height, boxes_per_row, rows_per_page, left_margin
         box_count += 1
 
         if box_string not in (NEWLINE_BOX,NEWPAGE_BOX):
-            page_output += render_box(box_dict, box_x_offset, box_y_offset, box_height, box_width, write_original_text)
+            page_output += render_box(box_dict, box_x_offset, box_y_offset, box_height, box_width, text_height_proportion, write_original_text)
 
         if box_count == boxes_per_row or box_string == NEWLINE_BOX:
             box_x_offset = left_margin
--- application/web_controller.py
+++ application/web_controller.py
@@ -15,6 +15,7 @@ DEFAULT_BOXES_PER_ROW = 40
 DEFAULT_ROWS_PER_PAGE = 30
 DEFAULT_LEFT_MARGIN = 60
 DEFAULT_TOP_MARGIN = 60
+DEFAULT_TEXT_HEIGHT_PERCENTAGE = 25
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -29,6 +30,7 @@ def main():
     rows_per_page = request.form.get('rows_per_page', DEFAULT_ROWS_PER_PAGE)
     left_margin = request.form.get('left_margin', DEFAULT_LEFT_MARGIN)
     top_margin = request.form.get('top_margin', DEFAULT_TOP_MARGIN)
+    text_height_percentage = request.form.get('text_height_percentage', DEFAULT_TEXT_HEIGHT_PERCENTAGE)
 
     numeric_string = request.form.get('numeric_string')
     translate_action = request.form.get('translate_action')
@@ -57,7 +59,7 @@ def main():
 
     if not translate_action is None:
         raw_translation = engine.process_text(input_text)
-        rendered_book = svg_builder.render_pages(raw_translation, int(box_height), int(boxes_per_row), int(rows_per_page), int(left_margin), int(top_margin), write_original_text)
+        rendered_book = svg_builder.render_pages(raw_translation, int(box_height), int(boxes_per_row), int(rows_per_page), int(left_margin), int(top_margin), int(text_height_percentage) ,write_original_text)
         pdf_page_list = []
         for i in range(1, len(rendered_book) + 1):
             rendered_page = rendered_book[i-1] 
@@ -77,7 +79,7 @@ def main():
         merger.close()
 
     print ('rendered_book len', len(rendered_book))
-    return render_template("main.html", text=input_text, book=rendered_book, book_length=len(rendered_book), box_height=box_height, boxes_per_row = boxes_per_row, rows_per_page = rows_per_page, left_margin=left_margin, top_margin=top_margin, checked_value=checked_value)
+    return render_template("main.html", text=input_text, book=rendered_book, book_length=len(rendered_book), box_height=box_height, boxes_per_row = boxes_per_row, rows_per_page = rows_per_page, left_margin=left_margin, top_margin=top_margin, checked_value=checked_value, text_height_percentage=text_height_percentage)
 
 @app.route('/about')
 def about():
