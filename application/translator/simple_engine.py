abd6ed04561e3cea9baed0e5ddb05b9daa878fc1
Mauricio Gomez <mauro.gomez@oracle.com>
2022-11-18 22:51:55 -0300

    Tratamiento de mayusculas

--- application/translator/simple_engine.py
+++ application/translator/simple_engine.py
@@ -16,11 +16,6 @@ def process_char(char):
     if char == TOKEN_NEWLINE:
         return [{'box': 'NL', 'desc': 'NL'}]
 
-    if char.isupper():
-        prefix = {'box': alphabet.uppercase_prefix, 'desc': alphabet.uppercase_description}
-    else:
-        prefix = None
-
     box_text = alphabet.alphabet_castillian.get(char.lower())
 
     if box_text is None:
@@ -28,10 +23,7 @@ def process_char(char):
     else:
         box = {'box': box_text, 'desc': char}
     
-    if not prefix is None:
-        return [prefix, box]
-    else:
-        return [box]
+    return [box]
 
 def process_token(token, previous_token):
     print(token)
@@ -47,21 +39,24 @@ def process_token(token, previous_token):
     else:
 
         output = []
+        prefix = []
 
         number_match = number_pattern.match(token)
 
-        if number_match is None:
-            prefix = None
+        if number_match is not None:
+            prefix = [{'box': alphabet.number_prefix, 'desc': alphabet.number_description}]
+
+        if token.isalpha() and token.isupper():
+            prefix = [{'box': alphabet.uppercase_prefix, 'desc': alphabet.uppercase_description}, {'box': alphabet.uppercase_prefix, 'desc': alphabet.uppercase_description}]
         else:
-            prefix = {'box': alphabet.number_prefix, 'desc': alphabet.number_description}
+            if token.isalpha() and token[0].isupper():
+                prefix = [{'box': alphabet.uppercase_prefix, 'desc': alphabet.uppercase_description}]
 
         output_char = []
         for char in token:
             output_char += process_char(char)
 
-        if not prefix is None:
-            output += [prefix]
-
+        output += prefix
         output += output_char
 
         return output
@@ -103,7 +98,7 @@ def process_text(text):
 
 if __name__ == "__main__":
 
-    text = 'Esto es el Ñandú número 20 en la lista.'
+    text = 'Esto es el Ñandú número 20 en la lista. MAYUSCULA Mayuscula Mayuscula2'
 
     output_list = process_text(text)
 
